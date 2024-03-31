064faf24fc4e721d9d2ef7b747bfbb3a5beb3c1d
lalithkishore <lalithkishore.sridhar@gmail.com>
2024-03-31 13:07:07 +0530

    user and coach login logic completed

--- coach_login/views.py
+++ coach_login/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
+
 
 # Create your views here.
 def coach_login(request):
-    return render(request,"coach_login.html")
+    if request.method=='POST':
+       username=request.POST['username']
+       password=request.POST['password']
+       user=auth.authenticate(username=username,password=password)
+       if user is not None and user.is_superuser:
+           auth.login(request,user)
+           return redirect("/")
+       else:
+           messages.info(request,'invalid credentials')
+           return redirect("coach_login")
+    else:
+       return render(request,"coach_login.html")
--- user_login/views.py
+++ user_login/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
 
+username=""
 # Create your views here.
 def archer_login(request):
-    return render(request,"user_login.html")
+    if request.method=='POST':
+       username=request.POST['username']
+       password=request.POST['password']
+       user=auth.authenticate(username=username,password=password)
+       if user is not None:
+           auth.login(request,user)
+           return redirect("/")
+       else:
+           messages.info(request,'invalid credentials')
+           return redirect("archer_login")
+    else:
+       return render(request,"user_login.html")
